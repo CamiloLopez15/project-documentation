688a32d8cb76a1441bd98bc0d77d8df96077bda1
Camilo López <camilolopez1506@gmail.com>
2024-04-15 09:20:32 -0500

    Feat: added menu responsive, logo, typography and icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,34 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import animations from "tailwindcss-animated";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary-dark-gray": "#C4C4C4",
+                "primary-dark-orange": "#FCA311",
+                "primary-light-blue": "#339AF0",
+            },
+            fontFamily: {
+                KeniaOne: ["KeniaOne", ...defaultTheme.fontFamily.sans],
+            },
+            backgroundColor: {
+                "primary-dark": "#FCA311",
+                "primary-light": "#339AF0",
+            },
+            keyframes: {
+                reflection: {
+                    "0%, 100%": { transform: "scaleX(1)" },
+                    "50%": { transform: "scaleX(-1)" },
+                },
+            },
+            animation: {
+                reflection: 'reflection 0.5s ease-in-out',
+              }
+        },
+    },
+    plugins: [animations],
+};
